0d507da2c584eac718314317644839d41ffa0a9f
Devv3n <s24000006539@leggott.ac.uk>
2024-11-15 16:35:06 +0000

    Add project files.

---
+++ Form.cs
@@ -0,0 +1,7 @@
+namespace Blue_Lagoon___Chaos_Edition__SERVER_ {
+    public partial class Form : System.Windows.Forms.Form {
+        public Form() {
+            InitializeComponent();
+        }
+    }
+}
